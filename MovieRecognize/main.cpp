06fbe3867c72101b51c57d571aec4b794340956d
Anton Krasnokutskiy <krasnokutskiy.anton@gmail.com>
2015-11-02 18:53:07 +0200

    Initial Commit

---
+++ MovieRecognize/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MovieRecognize
+//
+//  Created by Anton Krasnokutskiy on 02.11.15.
+//  Copyright (c) 2015 Anton Krasnokutskiy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
